5212c01f2941d51f4bcadca0b0c4b197a1b272b7
Grace Tian <snowmonkey.gt@gmail.com>
2020-07-18 17:10:08 -0400

    Use new train/test split, show for loop for optimal max_depth

--- decision_tree.py
+++ decision_tree.py
@@ -4,7 +4,7 @@ from sklearn.metrics import (confusion_matrix,
     accuracy_score, precision_score, recall_score, f1_score)
 
 path = '/home/grace/ml_bootcamp/creditcard/' # path to local folder
-seed = 12 # for random_state
+seed = 18 # for random_state
 
 # Load train/test data from CSVs to dataframes
 X_train_path = path + 'train_features.csv'
@@ -17,24 +17,26 @@ y_train = pd.read_csv(y_train_path)
 X_test = pd.read_csv(X_test_path)
 y_test = pd.read_csv(y_test_path)
 
-# make decision tree classifier
-tree = DecisionTreeClassifier(max_depth=40, random_state=seed)
-tree.fit(X_train, y_train)
-y_trainer = tree.predict(X_train)
-y_pred = tree.predict(X_test)
-print("\nMax depth = {}".format(depth))
-# Confusion matrix
-print(confusion_matrix(y_test, y_pred))
+for depth in range(20,31):
+    print('Depth: {}'.format(depth))
+    # make decision tree classifier
+    tree = DecisionTreeClassifier(max_depth=depth, random_state=seed)
+    tree.fit(X_train, y_train)
+    y_trainer = tree.predict(X_train)
+    y_pred = tree.predict(X_test)
 
-# Score tree
-acc_train = accuracy_score(y_train, y_trainer)
-acc_test = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred)
+    # Score tree
+    print(confusion_matrix(y_train,y_trainer))
+    print(confusion_matrix(y_test, y_pred))
 
-print("Accuracy on training set: {}".format(acc_train))
-print("Accuracy on test set: {}".format(acc_test))
-print("The precision is {}".format(precision))
-print("The recall/sensitivity is {}".format(recall))
-print("The F1 score is {}".format(f1))
+    #acc_train = accuracy_score(y_train, y_trainer)
+    #acc_test = accuracy_score(y_test, y_pred)
+    #precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    #f1 = f1_score(y_test, y_pred)
+
+    #print("Accuracy on training set: {}".format(acc_train))
+    #print("Accuracy on test set: {}".format(acc_test))
+    #print("The precision is {}".format(precision))
+    print("The recall/sensitivity is {}".format(recall))
+    #print("The F1 score is {}".format(f1))
