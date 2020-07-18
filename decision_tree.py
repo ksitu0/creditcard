a63ce7ecefaeb0d34e6fa3186f36b8cdbbaa270d
Grace Tian <snowmonkey.gt@gmail.com>
2020-07-18 17:13:54 -0400

    found max_depth=27 is optimal

--- decision_tree.py
+++ decision_tree.py
@@ -17,26 +17,24 @@ y_train = pd.read_csv(y_train_path)
 X_test = pd.read_csv(X_test_path)
 y_test = pd.read_csv(y_test_path)
 
-for depth in range(20,31):
-    print('Depth: {}'.format(depth))
-    # make decision tree classifier
-    tree = DecisionTreeClassifier(max_depth=depth, random_state=seed)
-    tree.fit(X_train, y_train)
-    y_trainer = tree.predict(X_train)
-    y_pred = tree.predict(X_test)
+# make decision tree classifier
+tree = DecisionTreeClassifier(max_depth=27, random_state=seed)
+tree.fit(X_train, y_train)
+y_trainer = tree.predict(X_train)
+y_pred = tree.predict(X_test)
 
-    # Score tree
-    print(confusion_matrix(y_train,y_trainer))
-    print(confusion_matrix(y_test, y_pred))
+# Score tree
+print(confusion_matrix(y_train,y_trainer))
+print(confusion_matrix(y_test, y_pred))
 
-    #acc_train = accuracy_score(y_train, y_trainer)
-    #acc_test = accuracy_score(y_test, y_pred)
-    #precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    #f1 = f1_score(y_test, y_pred)
+acc_train = accuracy_score(y_train, y_trainer)
+acc_test = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
 
-    #print("Accuracy on training set: {}".format(acc_train))
-    #print("Accuracy on test set: {}".format(acc_test))
-    #print("The precision is {}".format(precision))
-    print("The recall/sensitivity is {}".format(recall))
-    #print("The F1 score is {}".format(f1))
+print("Accuracy on training set: {}".format(acc_train))
+print("Accuracy on test set: {}".format(acc_test))
+print("The precision is {}".format(precision))
+print("The recall/sensitivity is {}".format(recall))
+print("The F1 score is {}".format(f1))
