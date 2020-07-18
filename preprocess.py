59765a2e0f0d2c1e8d007a140648885d451450a5
Grace Tian <snowmonkey.gt@gmail.com>
2020-07-18 17:09:11 -0400

    made test_size=.333 so after SMOTE it becomes .2

--- preprocess.py
+++ preprocess.py
@@ -25,13 +25,15 @@ y = data_df['Class']
 print(y.value_counts())
 
 # Split into training and test sets (80/20)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.333, random_state=seed)
+
+print(y_test.value_counts())
 
 # Balance data with SMOTE
 sm = SMOTE(sampling_strategy=1.0, random_state=seed)
 X_train_res, y_train_res = sm.fit_sample(X_train,y_train) 
 
-print(y_res.value_counts())
+print(y_train_res.value_counts())
 
 # Save train/test from dataframe to CSV
 X_train_res.to_csv(path + 'train_features.csv', index=False)
