24b0dfd61934a3b5ccbbaf9f2d4b56a9974a19f1
ksitu0 <59377604+ksitu0@users.noreply.github.com>
2020-07-22 19:40:23 -0700

    num_estimators optimized

--- random_forest.py
+++ random_forest.py
@@ -1,11 +1,7 @@
+# Data 
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import (confusion_matrix, 
-    accuracy_score, precision_score, recall_score, f1_score)
-import pickle
 
-path = '/home/grace/ml_bootcamp/creditcard/' # path to local folder
-seed = 18 # for random_state
+path = ".../creditcard/"
 
 # Load train/test data from CSVs to dataframes
 X_train_path = path + 'train_features.csv'
@@ -14,17 +10,26 @@ X_test_path = path + 'test_features.csv'
 y_test_path = path + 'test_labels.csv'
 
 X_train = pd.read_csv(X_train_path)
-y_train = pd.read_csv(y_train_path)
+y_train = pd.read_csv(y_train_path).values.ravel()
 X_test = pd.read_csv(X_test_path)
-y_test = pd.read_csv(y_test_path)
+y_test = pd.read_csv(y_test_path).values.ravel()
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (confusion_matrix, 
+    accuracy_score, precision_score, recall_score, f1_score)
+import pickle
+
+seed = 42
 
 # Make random forest classifier
-forest = RandomForestClassifier(n_estimators=100, random_state=seed)
+forest = RandomForestClassifier(n_estimators=800, random_state=seed)
+print("fitting")
 forest.fit(X_train, y_train)
+
+print("predicting")
 y_trainer = forest.predict(X_train)
 y_pred = forest.predict(X_test)
 
-# Score tree
 print(confusion_matrix(y_train,y_trainer))
 print(confusion_matrix(y_test, y_pred))
 
@@ -41,7 +46,7 @@ print("The recall/sensitivity is {}".format(recall))
 print("The F1 score is {}".format(f1))
 
 # Save model and predictions
-#pickle.dump(forest, open("forest_model.pkl","wb"))
+pickle.dump(forest, open("forest_model.pkl","wb"))
 
-#forest_results = pd.DataFrame(y_pred)
-#forest_results.to_csv(path + 'forest_results.csv', index=False)
+forest_results = pd.DataFrame(y_pred)
+forest_results.to_csv(path + 'forest_results.csv', index=False)
